58de508cf6864171b888e1b16c1c034025e20a50
SypherDev <SypherXN@gmail.com>
2016-05-25 18:39:56 -0700

    Shockwave and little import organization

--- src/com/sxn/potionce/enchants/Shockwave.java
+++ src/com/sxn/potionce/enchants/Shockwave.java
@@ -2,7 +2,7 @@ package com.sxn.potionce.enchants;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.LivingEntity;
-import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.plugin.Plugin;
 
 import com.rit.sucy.service.SuffixGroups;
@@ -14,15 +14,15 @@ import com.sxn.potionce.data.ItemSets;
 /**
  * Pulls in all nearby enemies on hit
  */
-public class BlackHole extends ConfigurableEnchantment {
+public class Shockwave extends ConfigurableEnchantment {
 
     /**
      * Constructor
      *
      * @param plugin plugin reference
      */
-    public BlackHole(Plugin plugin) {
-        super(plugin, EnchantDefaults.BLACKHOLE, ItemSets.SBOWS.getItems(), 1, ConflictGroup.FORCE);
+    public Shockwave(Plugin plugin) {
+        super(plugin, EnchantDefaults.SHOCKWAVE, ItemSets.ARMOR.getItems(), 1, ConflictGroup.FORCE);
         description = "Damages and pulls in enemies on attack";
         suffixGroups.add(SuffixGroups.FORCE.getKey());
     }
@@ -36,13 +36,13 @@ public class BlackHole extends ConfigurableEnchantment {
      * @param event  event details
      */
     @Override
-    public void applyEffect(LivingEntity user, LivingEntity target, int level, EntityDamageByEntityEvent event) {
+    public void applyDefenseEffect(LivingEntity user, LivingEntity target, int level, EntityDamageEvent event) {
         if (roll(level)) {
             int range = range(level);
             for (Entity entity : user.getNearbyEntities(range, range, range)) {
                 if (works(entity, user)) {
                     ((LivingEntity) entity).damage(damage(level));
-                    entity.setVelocity(user.getLocation().subtract(entity.getLocation()).toVector().multiply(speed(level)));
+                    entity.setVelocity(user.getLocation().add(entity.getLocation()).toVector().multiply(speed(level)));
                 }
             }
         }
